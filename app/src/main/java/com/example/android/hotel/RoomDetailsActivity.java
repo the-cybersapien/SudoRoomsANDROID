6da6b299d5d3c8e03a737ce245bd74fc4e0f11ae
Aditya Aggarwal <aditya@cybersapien.xyz>
2017-03-26 05:20:17 +0530

    Added AsyncTask for Opening room for the Customer.

--- app/src/main/java/com/example/android/hotel/RoomDetailsActivity.java
+++ app/src/main/java/com/example/android/hotel/RoomDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.hotel;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +12,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RoomDetailsActivity extends AppCompatActivity {
 
@@ -23,12 +29,9 @@ public class RoomDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         received_uri = intent.getData();
-        if(received_uri != null)
-        {
+        if (received_uri != null) {
             setContentView(R.layout.activity_room_details_staff);
-        }
-        else
-        {
+        } else {
             setContentView(R.layout.activity_room_details);
         }
         mRoomImage = (ImageView) findViewById(R.id.room_image);
@@ -37,5 +40,64 @@ public class RoomDetailsActivity extends AppCompatActivity {
         Picasso.with(this).load("http://cdn.shopify.com/s/files/1/0257/6087/products/Grey_Single_Front_grande.png?v=1487375795").into(oval);
         mRoomImage.setAlpha(80);
 
+    }
+
+    private class RequestAccessAsyncTask extends AsyncTask<String, Void, Void> {
+
+        public static final String QUERY_URL = "192.168.2.214/sudorooms/customer/request.php";
+
+        @Override
+        protected Void doInBackground(String... params) {
+            if (params[0].isEmpty() || params[1].isEmpty()) {
+                return null;
+            }
+
+            Uri queryUri = Uri.parse(QUERY_URL)
+                    .buildUpon()
+                    .appendQueryParameter("room", params[0])
+                    .appendQueryParameter("action", params[1]).build();
+
+            URL connect = createURL(queryUri.toString());
+
+            int response = 0;
+            HttpURLConnection urlConnection = null;
+            if (connect != null) {
+                try {
+                    urlConnection = (HttpURLConnection) connect.openConnection();
+
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setConnectTimeout(10000);
+                    urlConnection.setReadTimeout(15000);
+
+                    urlConnection.connect();
+
+                    response = urlConnection.getResponseCode();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    urlConnection.disconnect();
+                }
+
+                if (response == 200 && params[1].equalsIgnoreCase("OPEN")) {
+                    Toast.makeText(RoomDetailsActivity.this, "Success! Opening Door now!", Toast.LENGTH_SHORT).show();
+                } else if (response == 200 && params[1].equals("CLOSE")) {
+                    Toast.makeText(RoomDetailsActivity.this, "Success! Closing Door now!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RoomDetailsActivity.this, "Error! Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return null;
+        }
+    }
+
+
+    private URL createURL(String x) {
+        try {
+            return new URL(x);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
